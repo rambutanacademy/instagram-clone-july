b0e786875d04c38cba85a67249217c51b2f60be3
F <fwrizalman@gmail.com>
2019-09-10 11:49:31 +0800

    Create photos

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,24 @@
+class PhotosController < ApplicationController
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = current_user.photos.new(photo_params)
+
+    if @photo.save
+      redirect_to @photo
+    else
+      render :new
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  private
+    def photo_params
+      params.require(:photo).permit(:caption, :user_id)
+    end
+end
